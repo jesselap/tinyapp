c1048b3f25cf07d9edce05737036167938dd6b87
Jesse <jesse.a.lapointe@gmail.com>
2021-05-10 19:57:17 +0000

    added page for each shortURL

--- express_server.js
+++ express_server.js
@@ -28,9 +28,20 @@ app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
-
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+// const urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// };
+
+// console.log(urlDatabase["b2xVn2"]);
